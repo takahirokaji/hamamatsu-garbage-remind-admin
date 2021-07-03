5303c7a7fc92ba317458fc428ce039fcb1b7056a
talahirokaji <tk69kj619@gmail.com>
2021-07-04 00:57:38 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+
+const CSVFileInput = (event: any) => {
+  const makeCSV = (csv: any) => {
+    console.log("run");
+    const tmp = csv.split("\n");
+
+    console.log(tmp);
+  };
+
+  const fileHandler = async (e: any) => {
+    if (!window.File && !window.FileReader && !window.FileList && !window.Blob)
+      return console.log("error");
+    const target = await e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsText(target);
+
+    reader.onload = (e: any) => {
+      const result = e.target.result;
+      makeCSV(result);
+    };
+    reader.onerror = function () {
+      alert("エラー：ファイルをロードできません。");
+    };
+  };
+  return <input type="file" accept=".csv" onChange={fileHandler} />;
+};
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>浜松ゴミ出しリマインダー:管理画面</p>
       </header>
+      <main className="App-main">
+        <div className="Garbage-container">
+          <div className="Garbage-container-title">
+            <p>ごみ・資源物分別品目一覧の更新</p>
+          </div>
+          <div className="Garbage-container-input">
+            <CSVFileInput />
+            <button>更新する</button>
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./Header.css";
+export const Header = () => {
+  return (
+    <div>
+      <header className="app-header">浜松ゴミ出しリマインダー管理画面</header>
+    </div>
+  );
+};
